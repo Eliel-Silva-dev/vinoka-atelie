4236b770ee4290e821c3add0b59bd3012876e98d
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-09-15 20:09:06 -0300

    feat: react vinoka components footer

---
+++ src/shared/components/Footer/index.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import Lgpd from '../Lgpd';
+import style from './style.min.module.css';
+import Link from 'next/link';
+
+const Footer = () => {
+  const [localStorage, setLocalStorage] = useState(true);
+
+  const date = new Date();
+  const year = date.getFullYear();
+
+  useEffect(() => {
+    const lsContent = window.localStorage.getItem('lgpdzeloimoveis');
+
+    if (!lsContent) {
+      setLocalStorage(false);
+    }
+  }, []);
+
+  return (
+    <footer className={style.footer}>
+      <p>
+        <Link href={'/privacyPolicies'}>Politica de privacidade</Link> -{' '}
+        <Link href={'/termsUse'}>Termos de uso</Link>
+      </p>
+      <p>Desenvolvido por Hydrah Tecnologia</p>
+      <p>&#9400; Todos os direitos reservados - {year}</p>
+
+      {!localStorage && <Lgpd />}
+    </footer>
+  );
+};
+export default Footer;
