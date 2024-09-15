45858db9351b5f1de1a6ba8b76bb10ca8e8b7c18
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-09-15 15:42:35 -0300

    feat: react vinoka pasta teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      
-    </div>
+    <main className={styles.main_home}>
+      <button>
+        <Link href='/teste'>pagina de teste</Link>
+      </button>
+    </main>
   );
 }
---
+++ src/app/teste/page.tsx
@@ -0,0 +1,15 @@
+import style from "./style.min.module.css";
+
+const Teste = () => {
+  return (
+    <main className={style.main_teste}>
+      <h2>Pagina de teste</h2>
+      <h2>Pagina de teste</h2>
+      <h2>Pagina de teste</h2>
+      <h2>Pagina de teste</h2>
+      <h2>Pagina de teste</h2>
+    </main>
+  );
+};
+
+export default Teste;
