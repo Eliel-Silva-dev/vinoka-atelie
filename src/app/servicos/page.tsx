74927a7638bcad66edd0e398cf09667f94fa49ad
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-09-16 16:29:22 -0300

    feat: react vinoka page service

--- src/app/servicos/page.tsx
+++ src/app/servicos/page.tsx
@@ -1,12 +1,64 @@
 import Contact from '@/shared/components/Contact';
 import style from './style.min.module.css';
+import { servicos } from '@/shared/database';
 
+type TServiceProps = {
+  nomeServico: string;
+  imgCardService: string;
+  itensServicos: {
+    nomeServico: string;
+    valorServico: string;
+  }[];
+};
 const Servicos = () => {
+  const CardServicos = ({
+    nomeServico,
+    imgCardService,
+    itensServicos,
+  }: TServiceProps) => {
+    return (
+      <div className={style.cardService}>
+        <div>
+          <img src={imgCardService} alt={nomeServico} />
+        </div>
+        <div>
+          <h2>{nomeServico}</h2>
+        </div>
+        <div className={style.service_container}>
+          {itensServicos.map((servico, idx) => {
+            return (
+              <div className={style.service_iten} key={idx}>
+                <span>{servico.nomeServico}</span>
+                <div></div>
+                <span>{servico.valorServico}</span>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  };
   return (
     <>
       <main className={style.main_servicos}>
         <h2>Serviços</h2>
-
+        <p>
+          Oferecemos os principais serviços para que as peças se encaixem
+          perfeitamente ao seu estilo.
+        </p>
+        <div className={style.container_servicos}>
+          {servicos &&
+            servicos.map((servico, idx) => {
+              return (
+                <CardServicos
+                  key={idx}
+                  imgCardService={servico.imgCardService}
+                  itensServicos={servico.itensServicos}
+                  nomeServico={servico.nomeServico}
+                />
+              );
+            })}
+        </div>
       </main>
       <Contact />
     </>
--- src/shared/database/index.tsx
+++ src/shared/database/index.tsx
@@ -1,27 +1,116 @@
 export const servicos = [
   {
-    nomeServico: 'Vestido',
+    nomeServico: 'Vestidos',
     imgCardService: '/img/imgcardservicosvestido.jpg',
-    itensServicos: ['Alça', 'Barra', 'Atrás', 'Lateral', 'Subir corpo'],
+    itensServicos: [
+      {
+        nomeServico: 'Alça',
+        valorServico: 'R$ ??',
+      },
+      {
+        nomeServico: 'Barra',
+        valorServico: 'R$ ??',
+      },
+      {
+        nomeServico: 'Atrás',
+        valorServico: 'R$ ??',
+      },
+      {
+        nomeServico: 'Lateral',
+        valorServico: 'R$ ??',
+      },
+      {
+        nomeServico: 'Subir_corpo',
+        valorServico: 'R$ ??',
+      },
+    ],
   },
   {
     nomeServico: 'Camisas',
     imgCardService: '/img/imgcardservicoscamisa.jpg',
-    itensServicos: ['Alça', 'Barra', 'Atrás', 'Lateral', 'Subir corpo'],
+    itensServicos: [
+      {
+        nomeServico: 'Punho',
+        valorServico: 'R$ ??',
+      },
+      {
+        nomeServico: 'Ombro',
+        valorServico: 'R$ ??',
+      },
+      {
+        nomeServico: 'Lateral',
+        valorServico: 'R$ ??',
+      },
+      {
+        nomeServico: 'Comprimento',
+        valorServico: 'R$ ??',
+      },
+      {
+        nomeServico: 'Trocar_botão',
+        valorServico: 'R$ ??',
+      },
+    ],
   },
   {
     nomeServico: 'Blusas',
     imgCardService: '/img/imgcardservicosblusa.jpg',
-    itensServicos: ['Alça', 'Barra', 'Atrás', 'Lateral', 'Subir corpo'],
+    itensServicos: [
+      {
+        nomeServico: 'Alça',
+        valorServico: 'R$ ??',
+      },
+      {
+        nomeServico: 'Barra',
+        valorServico: 'R$ ??',
+      },
+      {
+        nomeServico: 'Ombro',
+        valorServico: 'R$ ??',
+      },
+      {
+        nomeServico: 'Lateral',
+        valorServico: 'R$ ??',
+      },
+    ],
   },
   {
     nomeServico: 'Ziper',
     imgCardService: '/img/imgcardservicosziper.jpg',
-    itensServicos: ['Alça', 'Barra', 'Atrás', 'Lateral', 'Subir corpo'],
+    itensServicos: [
+      {
+        nomeServico: 'Saias',
+        valorServico: 'R$ ??',
+      },
+      {
+        nomeServico: 'Calças',
+        valorServico: 'R$ ??',
+      },
+      {
+        nomeServico: 'Vestidos',
+        valorServico: 'R$ ??',
+      },
+      {
+        nomeServico: 'Jaquetas',
+        valorServico: 'R$ ??',
+      },
+    ],
   },
   {
     nomeServico: 'Calças',
     imgCardService: '/img/imgcardservicoscalca.jpg',
-    itensServicos: ['Alça', 'Barra', 'Atrás', 'Lateral', 'Subir corpo'],
+    itensServicos: [
+      {
+        nomeServico: 'Barra',
+        valorServico: 'R$ ??',
+      },
+      {
+        nomeServico: 'Funilar',
+        valorServico: 'R$ ??',
+      },
+      {
+        nomeServico: 'Cintura',
+        valorServico: 'R$ ??',
+      },
+    ],
   },
 ];
