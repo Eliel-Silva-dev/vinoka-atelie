dcac351631236208bd0a04b0d3c08ad2dc9296e5
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-09-16 00:09:28 -0300

    feat: react vinoka fundo home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <main className={styles.main_home}>
-      <h2>Página principal</h2>
+      <h2>
+        Não há necessidade de se vestir como todo mundo. É muito mais divertido
+        criar seu próprio visual.
+      </h2>
     </main>
   );
 }
