378cf99b924f5ba1fd258c23299cf67c31c27416
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-09-16 23:12:22 -0300

    feat: react vinoka service vitrine

---
+++ src/shared/services/api/intex.ts
@@ -0,0 +1,1 @@
+export * as VitrineServices from './vitrine/VitrineServices';
---
+++ src/shared/services/api/vitrine/VitrineServices.ts
@@ -0,0 +1,70 @@
+import { Environment } from '@/shared/environment';
+import { Api } from '../axios-config';
+
+export interface IListagemVitrine {
+  id: string;
+  typeModa: string;
+  imgsCardVitrine: string;
+  description: string;
+  imgsCarrossel: string[];
+}
+
+export interface IDetalheVitrine {
+  id: string;
+  typeModa: string;
+  imgsCardVitrine: string;
+  description: string;
+  imgsCarrossel: string[];
+}
+
+type TVitrineComTotalCount = {
+  data: IListagemVitrine[];
+  totalCount: number;
+};
+
+const getAllImoveis = async (
+  page = 1,
+  filter = '',
+  id = '',
+): Promise<TVitrineComTotalCount | Error> => {
+  try {
+    const urlRelativa = `/vitrine?_page=${page}&_limit=${Environment.LIMITE_BUSCA_API}&typeModa=${filter}&id_like=${id}`;
+    const { data, headers } = await Api.get(urlRelativa);
+
+    if (data) {
+      return {
+        data,
+        totalCount: Number(
+          headers['x-total-count'] || Environment.LIMITE_BUSCA_API,
+        ),
+      };
+    }
+
+    return new Error('Erro ao listar os registro');
+  } catch (error) {
+    console.error(error);
+
+    return new Error(
+      (error as { message: string }).message || 'Erro ao listar os registros',
+    );
+  }
+};
+
+const getImovelById = async (id: string): Promise<IDetalheVitrine | Error> => {
+  try {
+    const { data } = await Api.get<IDetalheVitrine>(`/vitrine/${id}`);
+    if (data) {
+      return data;
+    }
+
+    return new Error('Erro ao consultar o registro');
+  } catch (error) {
+    console.error(error);
+
+    return new Error(
+      (error as { message: string }).message || 'Erro ao consultar o registro',
+    );
+  }
+};
+
+export { getAllImoveis, getImovelById };
