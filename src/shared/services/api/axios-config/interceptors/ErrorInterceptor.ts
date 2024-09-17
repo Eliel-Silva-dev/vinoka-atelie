95df43c6d9039ca96a510a8893b2a1cd83b8250c
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-09-16 22:54:36 -0300

    feat: react vinoka db interceptors

---
+++ src/shared/services/api/axios-config/interceptors/ErrorInterceptor.ts
@@ -0,0 +1,13 @@
+import { AxiosError } from 'axios';
+
+export const errorInterceptor = (error: AxiosError) => {
+  if (error.message === 'Network Error') {
+    return Promise.reject(new Error('Erro de conexão'));
+  }
+
+  if (error.response?.status === 401) {
+    // Do something - 401 credenciais desatualizadas ou ausentes
+  }
+
+  return Promise.reject(error);
+};
