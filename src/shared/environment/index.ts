689219155148e34a16136cf335debe023b52ad47
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-09-16 23:13:40 -0300

    feat: react vinoka environment

---
+++ src/shared/environment/index.ts
@@ -0,0 +1,10 @@
+export const Environment = {
+  /*
+    limite de dados retornados na busca:
+  */
+  LIMITE_BUSCA_API: 20,
+  /*
+    url base das consultas api
+  */
+  URL_BASE: 'http://localhost:3333',
+};
