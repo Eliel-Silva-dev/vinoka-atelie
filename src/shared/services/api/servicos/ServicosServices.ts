763a4743f083740649f033768850072921e29a95
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-09-16 23:23:46 -0300

    feat: react vinoka service servicos

---
+++ src/shared/services/api/servicos/ServicosServices.ts
@@ -0,0 +1,71 @@
+import { Environment } from '@/shared/environment';
+import { Api } from '../axios-config';
+
+export interface IListagemServicos {
+  nomeServico: string;
+  imgCardService: string;
+  itensServicos: {
+    nomeServico: string;
+    valorServico: string;
+  }[];
+}
+
+export interface IDetalheServicos {
+  nomeServico: string;
+  imgCardService: string;
+  itensServicos: {
+    nomeServico: string;
+    valorServico: string;
+  }[];
+}
+
+type TServicosComTotalCount = {
+  data: IListagemServicos[];
+  totalCount: number;
+};
+
+const getAllImoveis = async (
+  page = 1,
+  id = '',
+): Promise<TServicosComTotalCount | Error> => {
+  try {
+    const urlRelativa = `/servicos?_page=${page}&_limit=${Environment.LIMITE_BUSCA_API}&typeModa=${filter}&id_like=${id}`;
+    const { data, headers } = await Api.get(urlRelativa);
+
+    if (data) {
+      return {
+        data,
+        totalCount: Number(
+          headers['x-total-count'] || Environment.LIMITE_BUSCA_API,
+        ),
+      };
+    }
+
+    return new Error('Erro ao listar os registro');
+  } catch (error) {
+    console.error(error);
+
+    return new Error(
+      (error as { message: string }).message || 'Erro ao listar os registros',
+    );
+  }
+};
+
+const getImovelById = async (id: string): Promise<IDetalheServicos | Error> => {
+  try {
+    const { data } = await Api.get<IDetalheServicos>(`/servicos/${id}`);
+    if (data) {
+      return data;
+    }
+
+    return new Error('Erro ao consultar o registro');
+  } catch (error) {
+    console.error(error);
+
+    return new Error(
+      (error as { message: string }).message || 'Erro ao consultar o registro',
+    );
+  }
+};
+
+export { getAllImoveis, getImovelById };
