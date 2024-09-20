97c8f3c79d98a6bb70f02d8ea16441dffeabcecd
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-09-20 16:49:58 -0300

    fix: react vinoka ajuste de alinhamento page detalhe

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -9,14 +9,6 @@ const Sobre = () => {
           <h2>Sobre nós</h2>
           <div>
             <h3>Atelie vinoka campinas</h3>
-            <p>
-              Ateliê Vinoka é uma profissional na arte de corte e costura,
-              especialista em dar nova vida a todos os tipos de roupas e tecidos
-              para todos estilos e ocasiões. Com anos de experiência em Campinas,
-              pode ajudar nos reparos, customizações, confecções entre outros.
-            </p>
-            <p>Confecção de peças de alto padrão, de acordo com as necessidades do cliente.</p>
-            
           </div>
         </section>
       </main>
