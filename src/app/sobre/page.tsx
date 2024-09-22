599db0deb317157a02c3fc23363245a86756779d
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-09-22 18:32:49 -0300

    feat: react vinoka add page sobre

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -8,7 +8,33 @@ const Sobre = () => {
         <section>
           <h2>Sobre nós</h2>
           <div>
-            <h3>Atelie vinoka campinas</h3>
+            <p>
+              Vinoka ateliê atua desde 2019. Criado por Valdivina Mota, técnica
+              em moda que procura sempre aprender para melhor atender aos
+              anseios dos cliente.
+            </p>
+            <p>
+              Trabalha-mos na confecção de peças casuais, sociais, esportivas e
+              nos reparos das mesma.
+            </p>
+            <p>
+              Procuramos trabalhar cada peça com cuidado e carinho como se nosso
+              fosse, por que cada cliente é especial.
+            </p>
+            <p className={style.right}>Sejam todos bem vindos!</p>
+            <p className={style.right}>Muito obrigado.</p>
+            <blockquote>
+              <p>
+                <cite>
+                  Porque Deus amou o mundo de tal maneira, que deu seu filho
+                  unigênito, para que todo aquele que nele crer, não pereça mas
+                  tenham vida eterna.
+                </cite>
+              </p>
+              <p>
+                <cite>João: 3.17</cite>
+              </p>
+            </blockquote>
           </div>
         </section>
       </main>
